e044b4dce54386edb5457ae474595aaec0a3bfb4
Valik0805 <kirillotohin@yandex.ru>
2020-09-18 15:41:55 +0700

    Добавьте файлы проекта.

---
+++ HelloEntity/ApplicationContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloEntity
+{
+    class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public ApplicationContext()
+            {
+                Database.EnsureCreated();
+            }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=HelloEntityDb;Trusted_Connection=true");
+        }
+
+    }
+}
---
+++ HelloEntity/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace HelloEntity
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (ApplicationContext db = new ApplicationContext()) { 
+                User user1 = new User { Name = "Kirill", Age = 19 };
+                User user2 = new User { Name = "Polina", Age = 19 };
+
+                db.Users.Add(user1);
+                db.Users.Add(user2);
+                db.SaveChanges();
+                Console.WriteLine("Объекты успешно добавлены");
+
+                var users = db.Users.ToList();
+                Console.WriteLine("Список объектов");
+                foreach(User u in users)
+                {
+                    Console.WriteLine($"{u.Id}.{u.Name} - {u.Age}");
+                }
+            }
+
+
+        }
+    }
+}
